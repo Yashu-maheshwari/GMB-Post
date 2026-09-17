451bf478cb5c70e6455e97463faa793d86ceadbc
Yashu-maheshwari <cute.yashu09@gmail.com>
2026-09-17 13:30:44 +0530

    chore: clean up temporary script tools

--- prep.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('google-apps-script/Code.js', 'utf8');
-
-// I will just replace the exact end of the resolve function.
-// Since it's currently messed up, I will just checkout and do it via exact string replace.
